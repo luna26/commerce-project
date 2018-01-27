cc016646a01dec72b3558708983a5b650a69d0cf
luna26 <33940028+luna26@users.noreply.github.com>
2018-01-26 18:36:11 -0600

    splash and icon

--- commerceProject/src/components/auth/Login.js
+++ commerceProject/src/components/auth/Login.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import LinearGradient from 'react-native-linear-gradient';
+import SplashScreen from 'react-native-splash-screen';
 import { Text, View } from 'react-native';
 import HeaderLogin from './HeaderLogin';
 import LoginForm from './LoginForm';
 import {Background} from '../common';
 
 class Login extends Component {
+    componentDidMount(){
+        setTimeout(() =>{
+            SplashScreen.hide();
+        }, 4000);
+    }
     render() {
         const { container } = styles;
         return (
@@ -18,6 +24,8 @@ class Login extends Component {
         );
     }
 }
+
+//android:background="@mipmap/launch_screen"
 
 const styles = {
     container:{
